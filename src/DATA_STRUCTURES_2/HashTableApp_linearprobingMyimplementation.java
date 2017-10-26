9ad8de2236a321c63f8acf6ceab39f38ece562c4
pejman <naderpejman@yahoo.com>
2017-10-26 11:16:36 -0600

    more

---
+++ src/DATA_STRUCTURES_2/HashTableApp_linearprobingMyimplementation.java
@@ -0,0 +1,35 @@
+package DATA_STRUCTURES_2;
+
+
+import java.io.*;
+ 
+class DataItemMine{
+	private int iData;
+	
+	public DataItemMine(int ii)//constructor 
+	{	
+		this.iData=ii;
+	}
+	
+	public int getKey(){
+		return this.iData;
+	}
+	
+}
+
+class HashTableMine {
+	
+	private DataItem[] hashArray; //this will hold our data item
+	
+	public HashTableMine(){
+		
+	}
+	
+}
+public class HashTableApp_linearprobingMyimplementation {
+	
+	public static void main (String args[]){
+		
+	}
+
+}
